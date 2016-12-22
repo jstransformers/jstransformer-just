jstransformers/jstransformer-htmling

Merge boilerplate

index.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
-var HTMLing = require('htmling');
+var HTMLing = require('htmling')
 
-exports.name = 'htmling';
-exports.outputFormat = 'html';
+exports.name = 'htmling'
+exports.outputFormat = 'html'
 
 exports.compile = function (str, options) {
-  var template = HTMLing.string(str, options);
+  var template = HTMLing.string(str, options)
   return function (locals) {
     return template.render(locals)
   }

jstransformers/jstransformer-just

index.js
@@ -1,26 +1,26 @@
-'use strict';
+'use strict'
 
-var Just = require('just');
-var Promise = require('promise');
+var Just = require('just')
+var Promise = require('promise')
 
-exports.name = 'just';
-exports.outputFormat = 'html';
+exports.name = 'just'
+exports.outputFormat = 'html'
 
 exports.renderAsync = function (str, options, locals) {
   // Prepare the options array.
-  options = options || {};
-  options['root'] = { page: str };
+  options = options || {}
+  options.root = {page: str}
 
   // Construct the template.
-  var template = new Just(options);
+  var template = new Just(options)
 
   // Build the Promise to render the template.
   return new Promise(function (resolve, reject) {
     template.render('page', locals, function (err, output) {
       if (err) {
-        return reject(err);
+        return reject(err)
       }
-      resolve(output);
-    });
-  });
-};
+      resolve(output)
+    })
+  })
+}